b80a04ab5ff814e80aab167a1a0f8b1095be891f
MTleen <673719729@qq.com>
2021-02-26 17:18:28 +0800

    使用 uni-app 重构小程序

---
+++ miniprogram_uni/pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<view class="container">
+			<u-button type='info' :ripple="true">授 权 登 陆</u-button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				userInfo: {},
+				hasUserInfo: false,
+				canIUse: uni.canIUse('button.open-type.getUserInfo'),
+				mode: "自动模式",
+				nextMode: "手动模式",
+				modes: {
+				  "自动模式": 1,
+				  "手动模式": 0
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		margin-top: 30%;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
